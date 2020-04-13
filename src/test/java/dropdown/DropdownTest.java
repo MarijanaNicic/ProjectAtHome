f4a8e870d329538b514bec8b74ac4d6dbc0ffb26
MarijanaNicic <MarijanaNicic@users.noreply.github.com>
2020-04-13 20:21:11 +0200

    Initial commit

---
+++ src/test/java/dropdown/DropdownTest.java
@@ -0,0 +1,20 @@
+package dropdown;
+
+import base.baseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class DropdownTest extends baseTests {
+    @Test
+    public void testSelectOption(){
+        var dropDownPage = homePage.clickDropDown();
+        String option = "Option 1";
+        dropDownPage.selectFromDropDown(option);
+        var selectedOptions = dropDownPage.getSelectedOption();
+        assertEquals(selectedOptions.size(),1,"Incorrect number of selections");
+        assertTrue(selectedOptions.contains(option), "Option not selected");
+
+
+    }
+}
