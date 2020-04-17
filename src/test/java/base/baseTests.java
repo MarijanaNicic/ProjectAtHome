1fb5b231241a69dff9238ed223cedbc1b341c602
MarijanaNicic <MarijanaNicic@users.noreply.github.com>
2020-04-17 18:45:44 +0200

    Screenshots and Event Listeners

--- src/test/java/base/baseTests.java
+++ src/test/java/base/baseTests.java
@@ -1,22 +1,32 @@
 package base;
 
-import org.openqa.selenium.WebDriver;
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
+
 public class baseTests {
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
-       // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
+        // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         goHome();
         homePage = new HomePage(driver);
 
@@ -30,6 +40,19 @@ public class baseTests {
     @AfterClass
     public void tearDown() {
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/test.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public WindowManager getWindowManager() {
